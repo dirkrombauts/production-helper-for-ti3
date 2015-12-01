1cb75b4336648c9f4f3ccac905960d2b6d535a3e
Dirk Rombauts <dirk.rombauts@questmaster.net>
2015-12-01 16:33:16 +0100

    Create a separate StepArgumentTransformation class

--- ProductionHelperForTI3.Specification.AutomationLayer/Bindings.cs
+++ ProductionHelperForTI3.Specification.AutomationLayer/Bindings.cs
@@ -1,10 +1,9 @@
 ﻿using TechTalk.SpecFlow;
 using ProductionHelperForTI3.Domain;
+using NFluent;
 
 namespace ProductionHelperForTI3.Specification.AutomationLayer
 {
-    using NFluent;
-
     [Binding]
     public class Bindings
     {
@@ -27,76 +26,6 @@ namespace ProductionHelperForTI3.Specification.AutomationLayer
             }
 
             this.productionRun.Produce(numberOfUnits, unit);
-        }
-
-        [StepArgumentTransformation]
-        public Unit CreateUnit(string nameOfUnits)
-        {
-            switch (nameOfUnits)
-            {
-                case "Ground Forces":
-                case "Ground Force":
-                    {
-                        return Units.GroundForce;
-                    }
-
-                case "Mechanized Unit":
-                case "Mechanized Units":
-                    {
-                        return Units.MechanizedUnit;
-                    }
-
-                case "PDS":
-                    {
-                        return Units.PDS;
-                    }
-
-                case "Space Dock":
-                case "Space Docks":
-                    {
-                        return Units.SpaceDock;
-                    }
-
-                case "Fighters":
-                case "Fighter":
-                    {
-                        return Units.Fighter;
-                    }
-
-                case "Destroyer":
-                case "Destroyers":
-                    {
-                        return Units.Destroyer;
-                    }
-
-                case "Cruiser":
-                    {
-                        return Units.Cruiser;
-                    }
-
-                case "Carrier":
-                case "Carriers":
-                    {
-                        return Units.Carrier;
-                    }
-
-                case "Dreadnought":
-                case "Dreadnoughts":
-                    {
-                        return Units.Dreadnought;
-                    }
-
-                case "War Sun":
-                case "War Suns":
-                    {
-                        return Units.WarSun;
-                    }
-
-                default:
-                    {
-                        return null;
-                    }
-            }
         }
 
         [Given(@"I have the '(.*)' Technology")]
