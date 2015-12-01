18751645f934a481def2c4d6dd2e353b995d249e
Dirk Rombauts <dirk.rombauts@questmaster.net>
2015-12-01 13:48:22 +0100

    Add business entities

---
+++ ProductionHelperForTI3.Domain/SpaceDock.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using ProductionHelperForTI3.Domain;
+
+namespace ProductionHelperForTI3.Specification
+{
+
+    public class SpaceDock
+    {
+    }
+}
---
+++ ProductionHelperForTI3.Domain/Technology.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace ProductionHelperForTI3.Domain
+{
+    public class Technology
+    {
+    }
+}
