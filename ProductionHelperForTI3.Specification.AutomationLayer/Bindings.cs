f4a8286fc839efde3a32ef52a369d4b842e8469b
Dirk Rombauts <dirk.rombauts@questmaster.net>
2015-12-01 12:54:28 +0100

    Add binding class

---
+++ ProductionHelperForTI3.Specification.AutomationLayer/Bindings.cs
@@ -0,0 +1,34 @@
+﻿using TechTalk.SpecFlow;
+
+namespace ProductionHelperForTI3.Specification.AutomationLayer
+{
+    [Binding]
+    public class Bindings
+    {
+        [When(@"I produce '(.*)' '(.*)'")]
+        public void WhenIProduce(int numberOfUnits, string nameOfUnits)
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [Given(@"I have the '(.*)' Technology")]
+        public void GivenIHaveTheTechnology(string technology)
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [Then(@"I have to pay '(.*)' resource")]
+        [Then(@"I have to pay '(.*)' resources")]
+        [Then(@"I have to pay '(.*)' resource\(s\)")]
+        public void ThenIHaveToPayResources(int numberOfResources)
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [Given(@"my Race is the '(.*)'")]
+        public void GivenMyRaceIsThe(string nameOfRace)
+        {
+            ScenarioContext.Current.Pending();
+        }
+    }
+}
